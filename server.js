9968c8aa8347171b668769d249a60b036c928203
RyanLamor <Ryanlamor@gmail.com>
2020-06-18 20:30:33 -0600

    updated prove09

--- server.js
+++ server.js
@@ -7,14 +7,18 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/getRate', getData)
-  .get('/', function (req, res) {res.send('Testing')} )
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 function getData(req,res){
   const weight = Number(req.query.weight);
   const mailType = req.query.mailType;
 
-  calcRate(res, weight, mailType);
+    var cost = 100;
+
+    const params = {weight: weight, mailType: mailType, cost: cost};
+
+    res.render('pages/postageRate', params);
+  //calcRate(res, weight, mailType);
 }
 
 function calcRate(res, weight, mailType){

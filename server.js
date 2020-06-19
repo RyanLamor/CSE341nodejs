46b856df9ee1994485d96e4ddd86134a3009ac85
RyanLamor <Ryanlamor@gmail.com>
2020-06-18 20:00:02 -0600

    updated prove09

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 8888
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 8888;
 
 express()
   .use(express.static(__dirname + 'public'))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/getRate', getData)
+  .get('/', function (req, res) {res.send('Testing')})
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 function getData(req,res){

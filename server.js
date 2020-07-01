b26bd78b3dc8fe1b32e5aa654d8e01a3c6a6135b
RyanLamor <Ryanlamor@gmail.com>
2020-07-01 16:04:23 -0600

    updated project

--- server.js
+++ server.js
@@ -40,6 +40,7 @@ function checkLogin(req,res){
   getPersonFromDb(email, pass, function(error, result) {
     if (error || result == null || result.length != 1) {
     			res.status(500).json({success: false, data: error});
+          console.log("Database connnection error: " + error);
     		} else {
     			const person = result[0];
     			res.status(200).json(person);
